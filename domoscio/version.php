18e272491ee57e5c730b2bf7856037a65c99f752
fearcraftor <flelong@live.fr>
2015-07-08 16:06:25 +0200

    fix version number

--- domoscio/version.php
+++ domoscio/version.php
@@ -25,7 +25,7 @@ if (!defined('MOODLE_INTERNAL')) {
     die('Direct access to this script is forbidden.');
 }
 
-$plugin->version   = 2015070829;
+$plugin->version   = 2015070800;
 $plugin->requires  = 2015051100;
 $plugin->component = 'mod_domoscio';
 $plugin->cron      = 0;
